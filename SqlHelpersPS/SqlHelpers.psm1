7c02e3aa796ef771075ecd125ef04d03a446f5de
Piotr Benetkiewicz <piotr@ais.pl>
2015-12-22 20:40:07 +0100

    first ugly working version of backup

--- SqlHelpersPS/SqlHelpers.psm1
+++ SqlHelpersPS/SqlHelpers.psm1
@@ -38,22 +38,22 @@ function Get-Function {
         return $paramDictionary
     }
 	PROCESS {
-		Write-Host "Hello"
-	}
-}
-
-function MyFunction2 {
-	PROCESS {
-		#$db = $svr.Databases['AdventureWorks']
-		#$dbname = $db.Name
-		#$dt = get-date -format yyyyMMddHHmmss
-		#$dbbk = new-object ('Microsoft.SqlServer.Management.Smo.Backup')
-		#$dbbk.Action = 'Database'
-		#$dbbk.BackupSetDescription = "Full backup of " + $dbname
-		#$dbbk.BackupSetName = $dbname + " Backup"
-		#$dbbk.Database = $dbname
-		#$dbbk.MediaDescription = "Disk"
-		#$dbbk.Devices.AddDevice($bdir + "\" + $dbname + "_db_" + $dt + ".bak", 'File')
-		#$dbbk.SqlBackup($svr)
+		$name = $PSBoundParameters.dbName
+		Write-Host $name
+		[void][reflection.assembly]::LoadWithPartialName('Microsoft.SqlServer.Smo')
+		$svr=new-object Microsoft.SqlServer.Management.Smo.Server('LOCALHOST\LOCAL2008')
+		$db = $svr.Databases[$name]
+		$dbname = $db.Name
+		$dt = get-date -format yyyyMMddHHmmss
+		
+		[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMOExtended')
+		$dbbk = new-object Microsoft.SqlServer.Management.Smo.Backup
+		$dbbk.Action = 'Database'
+		$dbbk.BackupSetDescription = "Full backup of " + $dbname
+		$dbbk.BackupSetName = $dbname + " Backup"
+		$dbbk.Database = $dbname
+		$dbbk.MediaDescription = "Disk"
+		$dbbk.Devices.AddDevice("c:\temp\" + $dbname + "_db_" + $dt + ".bak", 'File')
+		$dbbk.SqlBackup($svr)
 	}
 }
